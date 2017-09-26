232f21bb3db55f21687e22684f5c5c7e0f3e32e4
KnightOneAdmin <37442216knight@gmail.com>
2017-09-26 18:48:56 +0800

    *fix 1､val 修饰的变量不能改变  2､var  可以重新赋值 3､：String   Kotlin 会自动识别是 String 类型（类型推导）

--- src/main/kotlin/com/knight/kotlin/KotlinMain.kt
+++ src/main/kotlin/com/knight/kotlin/KotlinMain.kt
@@ -4,14 +4,19 @@ package com.knight.kotlin
  * Created by knightoneadmin on 2017/9/26.
  */
 
-val FINAL_HELLO_WORLD: String = "hello world"
+val FINAL_HELLO_WORLD: String = "hello world"//  val 修饰的 变量是不可变的  相当于  Java 的 final
 
-var value: String = "knight"
+var value: String = "knight"     // var 修饰的变量是可变的
 
 fun main(args: Array<String>) {
 
-//    FINAL_HELLO_WORLD="welcome"
-    value = "name"
-    println(FINAL_HELLO_WORLD) 
+//  FINAL_HELLO_WORLD="welcome"  // FINAL_HELLO_WORLD  因为是val修饰的，所以重新赋值是不可以的
+    value = "name"   //var  可以重新赋值
+
+    val MY_NAME = "my name is knight"  // 这里可以省轿略  ：String   Kotlin 会自动识别是 String 类型
+    val MY_NAME_AGE = 24
+    println(FINAL_HELLO_WORLD)
     println(value)
+    println("我的名字 =$MY_NAME,我的年龄 =$MY_NAME_AGE")
+
 }
