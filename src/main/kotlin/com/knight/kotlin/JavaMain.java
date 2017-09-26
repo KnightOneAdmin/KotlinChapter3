ed7d8606edc567a2efb2ef3f6eb1c8f34b52739a
KnightOneAdmin <37442216knight@gmail.com>
2017-09-26 18:10:31 +0800

    *fix first commit

---
+++ src/main/kotlin/com/knight/kotlin/JavaMain.java
@@ -0,0 +1,13 @@
+package com.knight.kotlin;
+
+/**
+ * Created by knightoneadmin on 2017/9/26.
+ */
+public class JavaMain {
+    public final String FINAL_HELL_WORLD = "hello world";
+
+    public static void main(String... args) {
+        String FINAL_HELL_WORLD= "my name is knight";
+        System.out.println(FINAL_HELL_WORLD);
+    }
+}
---
+++ src/main/kotlin/com/knight/kotlin/KotlinMain.kt
@@ -0,0 +1,17 @@
+package com.knight.kotlin
+
+/**
+ * Created by knightoneadmin on 2017/9/26.
+ */
+
+val FINAL_HELLO_WORLD: String = "hello world"
+
+var value: String = "knight"
+
+fun main(args: Array<String>) {
+
+//    FINAL_HELLO_WORLD="welcome"
+    value = "name"
+    println(FINAL_HELLO_WORLD) 
+    println(value)
+}
